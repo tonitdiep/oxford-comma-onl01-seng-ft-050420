e3c2e0332519970e7ee349f77232244d99485768
Toni  Diep <adiep123@gmail.com>
2020-05-06 02:59:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.count== 1
     return array[0]
   elsif array.count == 2   #add add in two-element array
-#     return array.join(" and ")
+     return array.join(" and ")
    else array.count >= 3
 #     return array[0..-2].join(", ") + ", and " + array[-1]   
 #     # (array[-1] with ",")    #add comma and "and" in a three-element array
